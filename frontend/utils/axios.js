760a99b811f55d2f1276dd83aafcb49aa87a6a5a
nittothomas <nittothomas94@example.com>
2025-03-11 14:04:08 +0530

    Change the localhost url to the serverhost url

--- frontend/utils/axios.js
+++ frontend/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://localhost:3000',
+  baseURL: 'https://todo-mern-backend-3o4v.onrender.com',
   timeout: 20000,
 });
 
